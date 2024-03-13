1cd14ddbed4704f582ad1d1179f4f1199ff42488
Julien Piccaluga <julien.piccaluga@kudelskisecurity.com>
2024-03-13 11:51:32 +0100

    fix version

--- install/install_monitoring.sh
+++ install/install_monitoring.sh
@@ -85,7 +85,7 @@ install_monitoring () {
 echo Do you want to install monitoring?
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) install_monitoring "${1:-latest}"; break;;
+        Yes ) install_monitoring "${1:v1.0.2}"; break;;
         No ) echo "### Aborting install. No changes are made on the system."; exit;;
     esac
 done
